cd4ba3adc33cb2cfb57127f0e5098710c850f52a
Nicholas Lamb <nickplamb@gmail.com>
2021-06-23 17:12:36 -0400

    change isFavorite function to use breed._id instead of whole object.

--- src/components/multi-breed-view/multi-breed-view.jsx
+++ src/components/multi-breed-view/multi-breed-view.jsx
@@ -7,19 +7,21 @@ import { BreedCard } from '../breed-card/breed-card';
 
 export function MultiBreedView({ breeds, token, favoriteBreeds }) {
 
-  // console.log(breeds.map(breed => breed.apaClass.name).filter((v, i, s) => s.indexOf(v)===i))
+
   return(
-    breeds.map(breed => 
-      (
-        <Col sm={10} md={6} lg={4} xl={3} className="mt-2" key={ breed._id }>
-          <BreedCard 
-            breed={ breed } 
-            token={ token } 
-            isFavorite={ (favoriteBreeds.indexOf(breed) > -1) ? true : false}
-          />
-        </Col>
-      )
-    )
+    breeds.map(breed => {
+      const isFavorite = (favoriteBreeds.map(favBreed => favBreed._id).indexOf(breed._id) > -1) ? true : false
+
+        return (
+          <Col sm={10} md={6} lg={4} xl={3} className="mt-2" key={ breed._id }>
+            <BreedCard 
+              breed={ breed } 
+              token={ token } 
+              isFavorite={ isFavorite }
+            />
+          </Col>
+        )
+    })
   )
 }
 
